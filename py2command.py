bca52195482d037bc7986836bd6e08db019187c3
TBUk <jonasargrill@gmail.com>
2022-03-19 01:15:31 -0500

    One command creations!
    py2command is now capable of generating one command creations! As a side effect, generated commands contain an obscene number of backslashes because escape quotes in NBT are stupid (like everything NBT related)

--- py2command.py
+++ py2command.py
@@ -1,9 +1,9 @@
+global generate_method
 command_order = []
-
 repeating = False
 
 
-def is_repeating(boolean):
+def is_repeating(boolean=False):
     global repeating
     if boolean:
         repeating = True
@@ -36,13 +36,18 @@ def nbt_parser(nbt):
                     name_lore = entry.split(',')
                     name = name_lore[0].replace('Name=', '')
                     nbt_listed.append(
-                        r"display:{Name:'" + '[{' + '"text":"' + name + '",' + '"italic"' + ":false}]'" + ",Lore:['" + '[{'
-                        + '"text":"' +
-                        name_lore[1] + '","italic":false}]' + "']}")
+                        r'display:{Name:\\\"' + '[{' + r'\\\\\\\"text\\\\\\\":\\\\\\\"' + name + r'\\\\\\\",' +
+                        r'\\\\\\\"italic\\\\\\\"' + r':false}]\\\"' + r',Lore:[\\\"' +
+                        '[{' + r'\\\\\\\"text\\\\\\\":\\\\\\\"' +
+                        name_lore[1] + r'\\\\\\\",\\\\\\\"italic\\\\\\\":false}]' + r'\\\"]}')
                     nbt_listed.append(',')
                 else:
                     name = entry.replace('Name=', '')
-                    nbt_listed.append(r"display:{Name:'"+'[{"text":"'+name+'","italic":false}]'+"'}")
+                    nbt_listed.append(r'display:{Name:\\\"' + r'[{\\\\\\\"text\\\\\\\":\\\\\\\"' + name +
+                                      r'\\\\\\\",'
+                                      r'\\\\\\\"italic '
+                                      r'\\\\\\\":false}]' +
+                                      r'\\\"}')
             elif entry.startswith('Enchant='):
                 if ',' in entry:
                     enchant = entry.replace('Enchant=', '')
@@ -51,10 +56,11 @@ def nbt_parser(nbt):
                     for enchantment in enchant_list:
                         enchant_amp = enchantment.split(':')
                         if item_startswith(enchant_amp[0]):
-                            nbt_listed.append('{id:"' + enchant_amp[0] + '",lvl:' + enchant_amp[1] + '}')
+                            nbt_listed.append(r'{id:\\\"' + enchant_amp[0] + r'\\\",lvl:' + enchant_amp[1] + '}')
                             nbt_listed.append(',')
                         else:
-                            nbt_listed.append('{id:"minecraft:' + enchant_amp[0] + '",lvl:' + enchant_amp[1] + '}')
+                            nbt_listed.append(
+                                r'{id:\\\"minecraft:' + enchant_amp[0] + r'\\\",lvl:' + enchant_amp[1] + '}')
                             nbt_listed.append(',')
                     if nbt_listed[-1] == ',':
                         nbt_listed.pop()
@@ -64,11 +70,12 @@ def nbt_parser(nbt):
                     enchant = entry.replace('Enchant=', '')
                     enchant_amp = enchant.split(':')
                     if item_startswith(enchant_amp[0]):
-                        nbt_listed.append('Enchantments:[{id:"' + enchant_amp[0] + '",lvl:' + enchant_amp[1] + '}]')
+                        nbt_listed.append(
+                            r'Enchantments:[{id:\\\"' + enchant_amp[0] + r'\\\",lvl:' + enchant_amp[1] + '}]')
                         nbt_listed.append(',')
                     else:
                         nbt_listed.append(
-                            'Enchantments:[{id:"minecraft:' + enchant_amp[0] + '",lvl:' + enchant_amp[1] + '}]')
+                            r'Enchantments:[{id:\\\"minecraft:' + enchant_amp[0] + r'\\\",lvl:' + enchant_amp[1] + '}]')
                         nbt_listed.append(',')
             elif entry == 'Unbreakable':
                 nbt_listed.append('Unbreakable:1b')
@@ -117,14 +124,47 @@ def give(targets, item, nbt=None, count=1):
 
 
 def end_code(filename):
-    with open(filename + ".txt", "w") as file:
-        for command in command_order:
-            if command == command_order[0] and repeating:
-                file.write('[REPEAT] ')
-                file.write(command + '\n')
-            elif command == command_order[0] and repeating is not True:
-                file.write('[IMPULSE] ')
-                file.write(command + '\n')
-            else:
-                file.write('[CHAIN] ')
-                file.write(command + '\n')
+    if generate_method == "one_command":
+        run_int = 2
+        command_string = ''
+        print('\033[4;49;93mWARNING\033[0;49;93m:\033[0;49;97m One command creations are still in early testing. '
+              'There may be errors '
+              'when importing your commands.')
+        with open(filename + "_one_command.txt", "w") as file:
+            command_string += (
+                '/summon minecraft:falling_block ~2 ~2 ~ {Time:1,BlockState:{Name:redstone_block},Passengers:['
+                '{id:falling_block,Time:0,BlockState:{Name:dirt},Passengers:[{id:falling_block,Time:1,'
+                'BlockState:{Name:activator_rail}},')
+            for command in command_order:
+                if command == command_order[0] and repeating:
+                    command_string += ('{id:command_block_minecart,Command:"setblock ~' + str(
+                        run_int) + ' ~-1 ~ minecraft:repeating_command_block[facing=east]{Command:' + r'\"' + command +
+                                       r'\"}' + '"},')
+                    run_int += 1
+                elif command == command_order[0] and repeating is not True:
+                    command_string += ('{id:command_block_minecart,Command:"setblock ~' + str(
+                        run_int) + ' ~-1 ~ minecraft:command_block[facing=east]{Command:' + r'\"' + command + r'\"}' +
+                                       '"},')
+                    run_int += 1
+                else:
+                    command_string += ('{id:command_block_minecart,Command:"setblock ~' + str(
+                        run_int) + ' ~-1 ~ minecraft:chain_command_block[facing=east]{auto:1b,Command:' + r'\"' +
+                                       command
+                                       + r'\"}' + '"},')
+                    run_int += 1
+            command_string += '{id:command_block_minecart,Command:"kill @e[type=command_block_minecart]"},'
+            final_command_string = command_string[:-1]
+            final_command_string += ']}]}'
+            file.write(final_command_string)
+    if generate_method == "plaintext":
+        with open(filename + ".txt", "w") as file:
+            for command in command_order:
+                if command == command_order[0] and repeating:
+                    file.write('[REPEAT] ')
+                    file.write(command + '\n')
+                elif command == command_order[0] and repeating is not True:
+                    file.write('[IMPULSE] ')
+                    file.write(command + '\n')
+                else:
+                    file.write('[CHAIN] ')
+                    file.write(command + '\n')
